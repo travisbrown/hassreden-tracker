57408bc68132dbcf5a9fb57bd785493b6fa5b3c4
Travis Brown <travisrobertbrown@gmail.com>
2022-11-20 12:08:55 +0100

    Add Snowflake converter

--- projects/hst-tw-utils/src/lib.rs
+++ projects/hst-tw-utils/src/lib.rs
@@ -1,8 +1,24 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, TimeZone, Utc};
 
 const TWITTER_DATE_TIME_FMT: &str = "%a %b %d %H:%M:%S %z %Y";
 
 /// Parse the time format used in Twitter API responses.
 pub fn parse_date_time(input: &str) -> Result<DateTime<Utc>, chrono::ParseError> {
     Ok(DateTime::parse_from_str(input, TWITTER_DATE_TIME_FMT)?.into())
+}
+
+const FIRST_SNOWFLAKE: i64 = 250000000000000;
+
+fn is_snowflake(value: i64) -> bool {
+    value >= FIRST_SNOWFLAKE
+}
+
+pub fn snowflake_to_date_time(value: i64) -> Option<DateTime<Utc>> {
+    if is_snowflake(value) {
+        let timestamp_millis = (value >> 22) + 1288834974657;
+
+        Utc.timestamp_millis_opt(timestamp_millis).single()
+    } else {
+        None
+    }
 }
